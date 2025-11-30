6b4f6c8fbfa868227a60fa13d75b82224f0bc6ee
Shaikh Attaur Rahman <skattaurrahman@gmail.com>
2025-12-01 00:53:55 +0530

    Modified CORS file

--- config/cors.php
+++ config/cors.php
@@ -20,8 +20,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['http://localhost:5173'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://hiveshare.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
