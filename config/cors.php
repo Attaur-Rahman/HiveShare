dab20e81744a5746ce33cf7da734c155bf7a7ce4
Shaikh Attaur Rahman <skattaurrahman@gmail.com>
2025-12-01 01:02:49 +0530

    Fixed CORS file

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://hiveshare.vercel.app/'],
+    'allowed_origins' => ['https://hiveshare.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
